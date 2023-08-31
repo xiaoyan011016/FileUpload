0df48deadfb49816f0e7f071492038c5a20e18da
xiaoyan <2639776795@qq.com>
2023-08-31 17:35:12 +0800

    多文件上传

--- 文件上传客户端/文件上传客户端/js/upload.js
+++ 文件上传客户端/文件上传客户端/js/upload.js
@@ -303,3 +303,221 @@
     }
   });
 })();
+
+// 进度条管控
+(function () {
+  let upload4 = document.querySelector("#upload4"),
+    upload_inp = upload4.querySelector(".upload_inp"),
+    upload_inp_select = upload4.querySelector(".upload_button.select"),
+    upload_progress = upload4.querySelector(".upload_progress"),
+    upload_progress_value = upload_progress.querySelector(".value");
+  let _file = null;
+  const delay = (time) => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve();
+      }, time);
+    });
+  };
+  const isGo = (ele) => {
+    return (
+      ele.classList.contains("disable") || ele.classList.contains("loading")
+    );
+  };
+  // 给选择文件按钮绑定时间事件
+  upload_inp_select.addEventListener("click", async function () {
+    // 判断是否有disable或loading属性有就不继续往下走。类似节流防抖
+    if (isGo(this)) return;
+    //   触发自带的点击方法选取文件
+    upload_inp.click();
+  });
+
+  upload_inp.addEventListener("change", async (event) => {
+    event.preventDefault();
+    //   文件选取需要基于change事件完成
+    let file = upload_inp.files[0];
+    _file = file;
+    // 设置按钮样式
+    upload_inp_select.classList.add("loading");
+    try {
+      let fm = new FormData();
+      fm.append("file", _file);
+      fm.append("filename", _file.name);
+      let { code, codeText, servicePath } = await instance.post(
+        "/upload_single",
+        fm,
+        {
+          onUploadProgress: (e) => {
+            let { total, loaded } = e; //当前进度总数和进度现有位置
+            upload_progress.style.display = "block"; //开启显示容器
+            upload_progress_value.style.width = `${(loaded / total) * 100}%`;
+          },
+        }
+      );
+      if (+code === 0) {
+        upload_progress_value.style.width = `100%`; //成功直接显示100%
+        await delay(300);
+        alert(`上传成功:文件服务器地址${servicePath}`);
+        return;
+      }
+      throw codeText;
+    } catch (error) {
+      console.log(error);
+    } finally {
+      upload_inp_select.classList.remove("loading");
+      upload_progress.style.display = "none";
+      upload_progress_value.style.width = 0;
+    }
+  });
+})();
+
+// 多文件上传
+(function () {
+  let upload = document.querySelector("#upload5"),
+    upload_inp = upload.querySelector(".upload_inp"),
+    upload_inp_select = upload.querySelector(".upload_button.select"),
+    upload_inp_upload = upload.querySelector(".upload_button.upload"),
+    upload_list = upload.querySelector(".upload_list");
+  let _files = [];
+
+  const isGo = (ele) => {
+    return (
+      ele.classList.contains("disable") || ele.classList.contains("loading")
+    );
+  };
+  const handleStyle = (flag) => {
+    // flag为真代表选择文件按钮需要添加disable样式，上传服务器按钮需要添加loading样式
+    if (flag) {
+      upload_inp_select.classList.add("disable");
+      upload_inp_upload.classList.add("loading");
+    } else {
+      upload_inp_select.classList.remove("disable");
+      upload_inp_upload.classList.remove("loading");
+    }
+  };
+  // 给选择文件按钮绑定时间事件
+  upload_inp_select.addEventListener("click", function () {
+    // 判断是否有disable或loading属性有就不继续往下走。类似节流防抖
+    if (isGo(this)) return;
+    //   触发自带的点击方法选取文件
+    upload_inp.click();
+  });
+
+  // 生成唯一id函数，利用时间戳+随机数
+  const createRandom = () => {
+    let key = Date.now() * Math.random(); //因为每次的数都足够大都会带小数点不用后面进行判断
+    return String(key.toString(16)).replace(".", ""); // 转换为16进制后从数字转换为字符串,然后将点去除
+  };
+
+  upload_inp.addEventListener("change", async () => {
+    //   文件选取需要基于change事件完成
+    _files = Array.from(upload_inp.files); //FileList 类数组
+    // 重构数组，并设置唯一标识
+    _files = _files.map((file, index) => {
+      return {
+        file,
+        filename: file.name,
+        key: createRandom(),
+      };
+    });
+    //  显示已选文件
+    let str = "";
+    _files.forEach((item, index) => {
+      str += `
+          <li key='${item.key}'>
+            <span>文件：${item.filename}</span>
+            <span><em>移除</em></span>
+          </li>
+      `;
+    });
+    // 插入页面
+    upload_list.style.display = "block";
+    upload_list.innerHTML = str;
+  });
+  // 删除按钮的逻辑
+  upload_list.addEventListener("click", (e) => {
+    let target = e.target;
+    let curLi = null;
+    if (target.tagName === "EM") {
+      // 从结构中删除该元素的整体内容（删除li节点及其子节点）
+      let curLi = target.parentNode.parentNode;
+      if (!curLi) return; //不存在直接退出，防止出错
+      upload_list.removeChild(curLi);
+      // 删除数据
+      let key = curLi.getAttribute("key"); //通过自定义属性获取唯一标识
+      _files = _files.filter((file) => {
+        return key !== file.key;
+      });
+      if (_files.length === 0) {
+        upload_list.style.display = "none";
+        upload_list.innerHTML = "";
+      }
+    }
+  });
+
+  // 服务器上传按钮
+  upload_inp_upload.addEventListener("click", () => {
+    if (isGo(upload_inp_upload)) return;
+    if (_files.length === 0) {
+      alert("请先选择文件");
+      return;
+    }
+    handleStyle(true);
+    // 获取所有的li元素
+    let upload_list_arr = Array.from(upload_list.querySelectorAll("li"));
+    // 这里有多个请求需要发送
+    _files = _files.map((item) => {
+      let fm = new FormData();
+      fm.append("file", item.file);
+      fm.append("filename", item.filename);
+      // 进行当前li元素匹配,返回li元素
+      let curli = upload_list_arr.find((li) => {
+        return li.getAttribute("key") === item.key;
+      });
+      // 获取该li元素下的最后一个span元素
+      let curspan = curli?.querySelector("span:last-child");
+      return instance
+        .post("/upload_single", fm, {
+          // 给每一个文件设置进度条样式
+          onUploadProgress(ev) {
+            /*  这里设计让li元素的最后一个span元素中的内容变为进度百分比显示即可，因此在这里需要获取对于元素
+              只需要获取到当前的li元素即可，因为li元素身上绑定了唯一标识，因此可以获取所有的li元素和当map中item.key进行匹配
+              <li key='xx'>
+                    <span>文件：xxxxx</span>
+                    <span>进度显示：10%</span>
+              </li> 
+          */
+            if (curspan) {
+              curspan.innerHTML = `进度显示:${(
+                (ev.loaded / ev.total) *
+                100
+              ).toFixed(2)}%`;
+            }
+          },
+        })
+        .then((res) => {
+          if (+res.code === 0) {
+            if (curspan) {
+              curspan.innerHTML = `进度显示:100%`;
+              console.log("上传成功");
+              return;
+            }
+          }
+          Promise.reject(res.codeText);
+        });
+    });
+    Promise.all(_files) //只要有一个请求失败了整体失败
+      .then((res) => {
+        alert("上传成功");
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => {
+        handleStyle(false);
+        _files = [];
+        upload_list.style.display = "none";
+        upload_list.innerHTML = "";
+      });
+  });
+})();
